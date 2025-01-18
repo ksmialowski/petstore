5aa3c3d5d218676301d582ca11f50e0385f6dd6d
Kamil Smialowski <smialowski.kamil@gmail.com>
2025-01-18 15:41:48 +0100

    bootstrap js file instead of link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -56,7 +56,7 @@
         </p>
     </div>
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         const toastElement = document.querySelector('.toast');
